03e19dd2f668734d1f85177f616cad09ffdafcb5
egomann88 <justin-urbanek@bluewin.ch>
2024-06-16 21:45:03 +0200

    feat: TextCarousel Components

---
+++ Web/Models/CarouselWord.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models;
+
+public class CarouselWord
+{
+  public string Word { get; set; } = default!;
+  public string ImageUrl { get; set; } = string.Empty;
+  public string AltText { get; set; } = string.Empty;
+  public double AnimationDelay { get; set; }
+}
