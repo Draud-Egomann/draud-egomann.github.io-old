14676876e0709c3c70e80986d379b595aa7726fa
egomann88 <justin-urbanek@bluewin.ch>
2024-06-16 18:54:30 +0200

    feat: image gallery on mobile

---
+++ Web/Helper/HorizontalGalleryImageHelper.cs
@@ -0,0 +1,14 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public static class HorizontalGalleryImageHelper
+{
+  public static List<HorizontalGalleryImage> GetGalleryImages() {
+    return [
+      new() { Id = 1, ImageUrl = "https://loremflickr.com/320/240/dog", AltText = "Image 1", Position = "lg:-top-[22rem] lg:right-[13rem]", TranslateX = 0 },
+      new() { Id = 2, ImageUrl = "https://loremflickr.com/320/240/cat", AltText = "Image 2", Position = "lg:-top-[9rem] lg:right-[4rem]", TranslateX = 0 },
+      new() { Id = 3, ImageUrl = "https://loremflickr.com/320/240/paris", AltText = "Image 3", Position = "lg:top-[3rem] lg:right-[16rem]", TranslateX = 0 },
+    ];
+  }
+}
---
+++ Web/Models/HorizontalGalleryImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models;
+
+public class HorizontalGalleryImage
+{
+  public int Id { get; set; }
+  public string ImageUrl { get; set; } = default!;
+  public string AltText { get; set; } = string.Empty;
+  public string Position { get; set; } = default!;
+  public double TranslateX { get; set; }
+}
