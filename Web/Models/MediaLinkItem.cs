3130e414eb2fd0f665a2ba4a264e3331f15c0384
egomann88 <justin-urbanek@bluewin.ch>
2024-06-16 16:25:56 +0200

    refactor: dissource components

---
+++ Web/Models/MediaLinkItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models;
+
+public class MediaLinkItem
+{
+  public string ImageUrl { get; set; } = default!;
+
+  public string Title { get; set; } = default!;
+
+  public string? Description { get; set; }
+}
