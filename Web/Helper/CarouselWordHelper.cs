39947a701c9a63bd2b5dc8b018dc298f54660ef7
egomann88 <justin-urbanek@bluewin.ch>
2024-06-16 21:45:46 +0200

    feat: TextCarousel in About Page

---
+++ Web/Helper/CarouselWordHelper.cs
@@ -0,0 +1,16 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public static class CarouselWordHelper
+{
+  public static List<CarouselWord> GetCarouselWords() {
+    return [
+      new() { Word = "WordPress", ImageUrl = "https://placehold.co/64", AltText = "WordPress Logo", AnimationDelay = 0 },
+      new() { Word = "ASP .NET", ImageUrl = "https://placehold.co/64", AltText = "ASP .NET Logo", AnimationDelay = 0 },
+      new() { Word = "Ionic", ImageUrl = "https://placehold.co/64", AltText = "Ionic Logo", AnimationDelay = 0 },
+      new() { Word = "VueJS", ImageUrl = "https://placehold.co/64", AltText = "VueJS Logo", AnimationDelay = 0 },
+      new() { Word = "NuxtJS", ImageUrl = "https://placehold.co/64", AltText = "NuxtJS Logo", AnimationDelay = 0 }
+    ];
+  }
+}
