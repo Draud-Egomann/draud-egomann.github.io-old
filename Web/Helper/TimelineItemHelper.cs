eab274ce9c2151e79c03745e2442da746c94abe0
Justin <justin-urbanek@bluewin.ch>
2024-06-17 15:57:50 +0200

    feat: Timeline draft

---
+++ Web/Helper/TimelineItemHelper.cs
@@ -0,0 +1,28 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public static class TimelineItemHelper {
+  public static List<TimelineItem> GetTimelineItems() {
+    return [
+      new() {
+        Index = 1,
+        Year = "2019",
+        Title = "First item",
+        Description = "This is the first item in the timeline."
+      },
+      new() {
+        Index = 2,
+        Year = "2020",
+        Title = "Second item",
+        Description = "This is the second item in the timeline."
+      },
+      new() {
+        Index = 3,
+        Year = "2021",
+        Title = "Third item",
+        Description = "This is the third item in the timeline."
+      }
+    ];
+  }
+}
