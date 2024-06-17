5ce198fb3f433b7a499ddd0dec69596eb45506c5
Justin <justin-urbanek@bluewin.ch>
2024-06-17 15:57:50 +0200

    feat: Timeline draft

---
+++ Web/Models/TimelineItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Models;
+
+public class TimelineItem {
+  public int Index { get; set; }
+  public string Year { get; set; }
+  public string Title { get; set; }
+  public string Description { get; set; }
+}
