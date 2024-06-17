cbf5e6558dde7f7f230f116573cbbc08af3644f4
Justin <justin-urbanek@bluewin.ch>
2024-06-17 11:52:00 +0200

    feat: content grid 3

---
+++ Web/Models/MediaImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models;
+
+public class MediaImage
+{
+  public string ImageUrl { get; set; } = default!;
+  public string AltText { get; set; } = string.Empty;
+  public string Text { get; set; } = string.Empty;
+  public bool IsOverlayVisible { get; set; } = false;
+}
