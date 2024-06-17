59129eb5d784a648424cab46dc67693dfb2800da
Justin <justin-urbanek@bluewin.ch>
2024-06-17 11:52:00 +0200

    feat: content grid 3

---
+++ Web/Helper/MediaImageHelper.cs
@@ -0,0 +1,15 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public static class MediaImageHelper
+{
+  public static List<MediaImage> GetMediaImages() {
+    return [
+      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+    ];
+  }
+}
