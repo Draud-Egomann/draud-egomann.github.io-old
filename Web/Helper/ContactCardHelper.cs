5503667b9dc69ede9c2163550cfa561fc5ef8ba5
egomann88 <justin-urbanek@bluewin.ch>
2024-06-18 22:06:31 +0200

    feat: Contact Cards

---
+++ Web/Helper/ContactCardHelper.cs
@@ -0,0 +1,20 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public static class ContactCardHelper {
+  public static List<ContactCard> GetContactCards() {
+    return [
+      new() {
+        ImageUrl = "https://via.placeholder.com/800x300",
+        AltText = "Placeholder image",
+        Text = "This is a placeholder image"
+      },
+      new() {
+        ImageUrl = "https://via.placeholder.com/800x300",
+        AltText = "Placeholder image",
+        Text = "This is a placeholder image"
+      }
+    ];
+  }
+}
---
+++ Web/Models/ContactCard.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Models;
+
+public class ContactCard {
+  public string ImageUrl { get; set; } = default!;
+  public string AltText { get; set; } = default!;
+  public string Text { get; set; } = default!;
+}
