ee9fd9a2e1852fcb9a656f56392943f5ef409fdd
Justin <justin-urbanek@bluewin.ch>
2024-06-18 09:16:36 +0200

    feat: ProjectMenuItem Class with Helper

---
+++ Web/Helper/ProjectMenuItemHelper.cs
@@ -0,0 +1,24 @@
+﻿using Web.Models;
+
+namespace Web.Helper;
+
+public class ProjectMenuItemHelper {
+  public static List<ProjectMenuItem> GetProjectMenuItems() {
+    return [
+      new ProjectMenuItem {
+        Title = "Project 1",
+        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.",
+        HasMoreInfo = true,
+        MoreInfoText = "More Info",
+        Classes = []
+      },
+      new ProjectMenuItem {
+        Title = "Project 2",
+        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.",
+        HasMoreInfo = true,
+        MoreInfoText = "More Info",
+        Classes = []
+      }
+    ];
+  }
+}
---
+++ Web/Models/ProjectMenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models;
+
+public class ProjectMenuItem {
+  public string Title { get; set; } = default!;
+  public string Description { get; set; } = default!;
+  public bool HasMoreInfo { get; set; } = default!;
+  public string? MoreInfoText { get; set; }
+  public List<string> Classes { get; set; } = [];
+}
