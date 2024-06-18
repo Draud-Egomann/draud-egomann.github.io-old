85b95e78be2f3faaff0d118adc39b798161c616b
Justin <justin-urbanek@bluewin.ch>
2024-06-18 12:02:27 +0200

    feat: Slide

---
+++ Web/Models/Slide.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Models;
+
+public class Slide {
+  public string ImageUrl { get; set; } = default!;
+  public string AltText { get; set; } = string.Empty;
+  public string Thumbnail { get; set; } = default!;
+  public string? Caption { get; set; }
+}
