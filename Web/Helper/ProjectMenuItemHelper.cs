5a92a1b1e3982624b702aea3ffddd468a5d78182
Justin <justin-urbanek@bluewin.ch>
2024-06-18 09:33:28 +0200

    feat: Card and Cards Components

--- Web/Helper/ProjectMenuItemHelper.cs
+++ Web/Helper/ProjectMenuItemHelper.cs
@@ -5,14 +5,14 @@ namespace Web.Helper;
 public class ProjectMenuItemHelper {
   public static List<ProjectMenuItem> GetProjectMenuItems() {
     return [
-      new ProjectMenuItem {
+      new() {
         Title = "Project 1",
         Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.",
         HasMoreInfo = true,
         MoreInfoText = "More Info",
         Classes = []
       },
-      new ProjectMenuItem {
+      new() {
         Title = "Project 2",
         Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.",
         HasMoreInfo = true,
