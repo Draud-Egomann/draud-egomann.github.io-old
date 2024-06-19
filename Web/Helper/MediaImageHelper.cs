1d95bd86a5a313f13b909391c18bc3ebea844b13
Justin <justin-urbanek@bluewin.ch>
2024-06-19 21:08:17 +0200

    feat: own images

--- Web/Helper/MediaImageHelper.cs
+++ Web/Helper/MediaImageHelper.cs
@@ -6,10 +6,26 @@ public static class MediaImageHelper
 {
   public static List<MediaImage> GetMediaImages() {
     return [
-      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-      new () { ImageUrl = "https://via.placeholder.com/300", AltText = "Placeholder", Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+      new () {
+        ImageUrl = "./imgs/hobbies/write.jpg",
+        AltText = "Placeholder",
+        Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      },
+      new () {
+        ImageUrl = "./imgs/hobbies/program.webp",
+        AltText = "Placeholder",
+        Text = ""
+      },
+      new () {
+        ImageUrl = "./imgs/hobbies/gaming.webp",
+        AltText = "Gaming",
+        Text = ""
+      },
+      new () {
+        ImageUrl = "./imgs/hobbies/read.jpg",
+        AltText = "Placeholder",
+        Text = ""
+      },
     ];
   }
 }
