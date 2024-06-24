da470d732e6bf25a27b43a2097429bb5c6eadbb2
Justin <justin-urbanek@bluewin.ch>
2024-06-24 16:25:16 +0200

    feat: about me text

--- Web/Helper/HorizontalGalleryImageHelper.cs
+++ Web/Helper/HorizontalGalleryImageHelper.cs
@@ -6,9 +6,9 @@ public static class HorizontalGalleryImageHelper
 {
   public static List<HorizontalGalleryImage> GetGalleryImages() {
     return [
-      new() { Id = 1, ImageUrl = "https://loremflickr.com/320/240/dog", AltText = "Image 1", Position = "lg:-top-[22rem] lg:right-[13rem]", TranslateX = 0 },
-      new() { Id = 2, ImageUrl = "https://loremflickr.com/320/240/cat", AltText = "Image 2", Position = "lg:-top-[9rem] lg:right-[4rem]", TranslateX = 0 },
-      new() { Id = 3, ImageUrl = "https://loremflickr.com/320/240/paris", AltText = "Image 3", Position = "lg:top-[3rem] lg:right-[16rem]", TranslateX = 0 },
+      new() { Id = 1, ImageUrl = "./imgs/wolf.jpg", AltText = "Wolf", Position = "lg:-top-[22rem] lg:right-[13rem]", TranslateX = 0 },
+      new() { Id = 2, ImageUrl = "./imgs/workplace.webp", AltText = "Arbeitsort", Position = "lg:-top-[9rem] lg:right-[4rem]", TranslateX = 0 },
+      new() { Id = 3, ImageUrl = "./imgs/lucerne.jpg", AltText = "Image 3", Position = "lg:top-[3rem] lg:right-[16rem]", TranslateX = 0 },
     ];
   }
 }
