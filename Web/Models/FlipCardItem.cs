72f2ff2677b0cf9dec630c150055e281c8d0499d
Justin <justin-urbanek@bluewin.ch>
2024-06-26 12:10:27 +0200

    feat: FlipCards Component

---
+++ Web/Models/FlipCardItem.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Models;
+
+public class FlipCardItem {
+  public string FrontText { get; set; } = default!;
+  public string BgImage { get; set; } = default!;
+  public string? BackText { get; set; } = null;
+}
