757ccabe2d30bb22a3f03ceaea60d9e22e2e5240
Justin <justin-urbanek@bluewin.ch>
2024-07-03 11:19:33 +0200

    feat: Localization & Globalization

---
+++ Web/Services/CultureService.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Web.Services;
+
+public class CultureService {
+  public static List<CultureInfo> SupportedCultures { get; } = [
+    new CultureInfo("en-US"),
+    new CultureInfo("de-CH")
+  ];
+
+  public static CultureInfo CurrentCulture { get; set; } = SupportedCultures[1];
+  public static event Action OnCultureChanged;
+
+  public static void SetCulture(CultureInfo culture) {
+    if (!SupportedCultures.Contains(culture)) {
+      throw new NotSupportedException($"The culture '{culture.Name}' is not supported.");
+    }
+
+    CultureInfo.CurrentCulture = culture;
+    CultureInfo.CurrentUICulture = culture;
+    CurrentCulture = culture;
+    OnCultureChanged?.Invoke();
+  }
+}
