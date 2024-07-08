105298abb43fa499509ba4c06cb42935911c481f
Justin <justin-urbanek@bluewin.ch>
2024-07-08 09:53:56 +0200

    feat: links to card

--- Web/Models/ContactCard.cs
+++ Web/Models/ContactCard.cs
@@ -4,4 +4,6 @@ public class ContactCard {
   public string ImageUrl { get; set; } = default!;
   public string AltText { get; set; } = default!;
   public string Text { get; set; } = default!;
+  public bool IsLink { get; set; } = false;
+  public string? LinkUrl { get; set; }
 }
