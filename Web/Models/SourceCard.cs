757112c9296c359120ce09d865be0002207f78d3
Justin <justin-urbanek@bluewin.ch>
2024-07-09 11:44:26 +0200

    feat: Credits Data

---
+++ Web/Models/SourceCard.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models;
+
+public class SourceCard {
+  public string ImageUrl { get; set; }
+  public string AltText { get; set; } = string.Empty;
+  public string? ImageClass { get; set; }
+  public string? Title { get; set; }
+  public string Text { get; set; }
+  public string? SourceUrl { get; set; }
+}
