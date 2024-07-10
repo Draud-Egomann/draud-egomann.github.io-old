734ce52d7df4e53eff3502a29b20c4e7078056dc
Justin <justin-urbanek@bluewin.ch>
2024-07-10 15:54:44 +0200

    feat: Thumbnail in ImageItem

--- Web/Models/ImageReference.cs
+++ Web/Models/ImageReference.cs
@@ -1,8 +1,6 @@
 ﻿namespace Web.Models;
 
-public class ImageReference {
-  public string ImageUrl { get; set; }
-  public string AltText { get; set; } = string.Empty;
+public class ImageReference : ImageItem {
   public string? ProfileName { get; set; }
   public string? ProfileUrl { get; set; }
   public string SiteName { get; set; }
