be0f0542ece5cd6dd6f7c391b4404d7a89dd1c5a
Justin <justin-urbanek@bluewin.ch>
2024-07-10 13:20:42 +0200

    feat: Nest many Model into one single Model file

---
+++ Web/Models/Image.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Models;
+
+public class Image {
+  public string ImageUrl { get; set; } = default!;
+  public string? ImageClass { get; set; }
+  public string? AltText { get; set; }
+}
