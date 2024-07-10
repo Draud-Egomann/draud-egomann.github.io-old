5acfd526743262ec78886cac9a4c0de63ba2b323
Justin <justin-urbanek@bluewin.ch>
2024-07-10 15:54:44 +0200

    feat: Thumbnail in ImageItem

--- Web/Models/ImageItem.cs
+++ Web/Models/ImageItem.cs
@@ -4,4 +4,6 @@ public class ImageItem {
   public string ImageUrl { get; set; } = default!;
   public string? ImageClass { get; set; }
   public string? AltText { get; set; }
+  public string? ThumbnailUrl { get; set; }
+  public string? ThumbnailAltText { get; set; }
 }
