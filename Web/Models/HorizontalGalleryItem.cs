2db2b63e78138befd912f57ef1c701ad67416caf
Justin <justin-urbanek@bluewin.ch>
2024-07-10 14:50:42 +0200

    feat: rename Modal

--- Web/Models/HorizontalGalleryItem.cs
+++ Web/Models/HorizontalGalleryItem.cs
@@ -1,6 +1,6 @@
 ﻿namespace Web.Models;
 
-public class HorizontalGalleryImage : ImageItem {
+public class HorizontalGalleryItem : ImageItem {
   public int Id { get; set; }
   public string Position { get; set; } = default!;
   public double TranslateX { get; set; }
